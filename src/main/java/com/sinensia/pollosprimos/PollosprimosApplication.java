91389902f24e6e486df3e3c883a7473b34596350
Jordi <jordi@gmail.com>
2023-11-08 11:19:13 +0100

    commit inicial

---
+++ src/main/java/com/sinensia/pollosprimos/PollosprimosApplication.java
@@ -0,0 +1,13 @@
+package com.sinensia.pollosprimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollosprimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollosprimosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinensia/pollosprimos/PollosprimosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.pollosprimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollosprimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
