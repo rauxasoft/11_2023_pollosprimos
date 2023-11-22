5ea8a2500be4568825c98d9c248a1ed33e4304da
Jordi <jordi@gmail.com>
2023-11-22 11:20:24 +0100

    se quitan clases y metodos innecesarios

--- src/main/java/com/sinensia/pollosprimos/backend/integration/repositories/CamareroPLRepository.java
+++ src/main/java/com/sinensia/pollosprimos/backend/integration/repositories/CamareroPLRepository.java
@@ -12,26 +12,8 @@ public interface CamareroPLRepository extends JpaRepository<CamareroPL, Long>{
 	boolean existsByDni(String dni);
 	
 	CamareroPL findByDni(String dni);
-	
-	// Consultas con  JPQL
-	
+
 	@Query("SELECT c FROM CamareroPL c WHERE UPPER(c.nombre) LIKE UPPER(CONCAT('%',:texto,'%'))")
 	List<CamareroPL> findByNombreLikeIgnoreCase(String texto);
-	
-	@Query("SELECT c FROM CamareroPL c")
-	List<CamareroPL> dameTodos();
-	
-	@Query("SELECT c FROM CamareroPL c WHERE c.datosContacto.telefono = :telefono")
-	List<CamareroPL> damePorTelefono(String telefono);
-	
-	@Query("SELECT c.nombre, c.apellido1, c.apellido2, c.datosContacto FROM CamareroPL c")
-	List<Object[]> dameAtributosSueltos1();
-	
-	@Query("SELECT CONCAT(c.apellido1,' ',COALESCE(c.apellido2,''),', ',c.nombre), c.datosContacto FROM CamareroPL c")
-	List<Object[]> dameAtributosSueltos2();
-	
-	@Query("SELECT LENGTH(c.nombre), 2 + 2 , UPPER(c.nombre) FROM CamareroPL c")
-	List<Object[]> dameAtributosSueltos3();
-	
 	
 }
